1be7dc27f5ca91236100235dc7411096de2eb1ba
Thomas Dijkmans <tdijkmans@gmail.com>
2020-03-26 21:32:15 +0100

    DB upgrade

---
+++ recipefree/collector/admin.py
@@ -0,0 +1,6 @@
+from django.contrib import admin
+from .models import SmulWebPage, SmulWebRecipe
+
+# Register your models here.
+admin.site.register(SmulWebPage)
+admin.site.register(SmulWebRecipe)
